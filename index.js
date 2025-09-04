42b2b900e9056348af706fad001c5186cd01c450
Harshal <harsallaware121@gmail.com>
2025-09-04 16:29:57 +0530

    file updated

--- index.js
+++ index.js
@@ -19,7 +19,9 @@ app.get('/youtube', (req,res)=>{
 app.get('/login', (req,res)=>{
     res.send("<h1>This is login Page</h1>")
 })
-
+app.get('/home', (req,res)=>{
+    res.send("<h2>This is Home Page</h2>")
+})
 
 app.listen(process.env.PORT, () => {
   console.log(`Example app listening on port ${port}`)
